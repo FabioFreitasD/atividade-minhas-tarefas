9359595ec197fb17020263d258260e08d81f23ef
Fabio <fabiofreitascmav@gmail.com>
2025-02-16 15:11:27 -0300

    criando o estado dos botoes

--- src/Containers/ListaDeTarefas/index.tsx
+++ src/Containers/ListaDeTarefas/index.tsx
@@ -2,29 +2,41 @@ import Tarefa from '../../components/Tarefas'
 
 import * as S from './styles'
 
+const tarefas = [
+  {
+    titulo: 'Estudar Programação',
+    descricao: 'Ver aulas da faculdade',
+    prioridade: 'importante',
+    status: 'pendente'
+  },
+  {
+    titulo: 'Pagar a conta de Luz',
+    descricao: 'Pix via app',
+    prioridade: 'urgente',
+    status: 'concluido'
+  },
+  {
+    titulo: 'Pagar a internet',
+    descricao: 'Baixar a fatura',
+    prioridade: 'urgente',
+    status: 'pendente'
+  }
+]
+
 const ListaDeTarefas = () => (
   <S.Container>
     <p>2 Tarefas marcadas como: &quot;categoria&ldquo; e &quot;termo&ldquo;</p>
     <ul>
-      <li>
-        <Tarefa />
-      </li>
-
-      <li>
-        <Tarefa />
-      </li>
-
-      <li>
-        <Tarefa />
-      </li>
-
-      <li>
-        <Tarefa />
-      </li>
-
-      <li>
-        <Tarefa />
-      </li>
+      {tarefas.map((t) => (
+        <li key={t.titulo}>
+          <Tarefa
+            prioridade={t.prioridade}
+            descricao={t.descricao}
+            status={t.status}
+            titulo={t.titulo}
+          />
+        </li>
+      ))}
     </ul>
   </S.Container>
 )
